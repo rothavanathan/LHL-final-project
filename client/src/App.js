41f5551fb83524e51aca4cc81fa251e8cfc6b0ee
Laura Tannahill <lntannahill@gmail.com>
2021-01-15 22:26:29 +0000

    messing around w wavesurfer

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Player from './components/Player.js'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import Waveform from "./Waveform";
+import PlayList from "./components/PlayList";
+
+// const url = "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3";
+
+const tracks = [
+  {
+    id: 0,
+    title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
+    url:
+      "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3"
+  },
+  {
+    id: 1,
+    title: "Franz Schubert's Ständchen - Voice (Clarinet) & Piano",
+    url:
+      "https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3"
+  }
+];
+
+export default function App() {
+  const [selectedTrack, setSelectedTrack] = useState(tracks[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Player></Player>
-      </header>
+      <Waveform url={selectedTrack.url} />
+      <PlayList
+        tracks={tracks}
+        selectedTrack={selectedTrack}
+        setSelectedTrack={setSelectedTrack}
+      />
+      <br />
+      <p>Wavesurfer.js with React.JS</p>
     </div>
   );
 }
 
-export default App;
