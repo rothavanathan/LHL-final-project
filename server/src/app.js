78ed9e17b251b8de9748b71d1bea6bb2a4a43ac6
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 17:12:20 -0500

    add comments and require files

--- server/src/app.js
+++ server/src/app.js
@@ -8,18 +8,20 @@ const dbHelpers = require('./helpers/dbHelpers')(db);
 
 const stemsRouter = require("./routes/stems");
 const usersRouter = require("./routes/users");
+// Will need all relevant routes setup here
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
-// was originally false
+// was originally false below
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/stems", stemsRouter);
 app.use("/api/users", usersRouter(dbHelpers));
+// Other relevant api routes will come here
 
 module.exports = app;
