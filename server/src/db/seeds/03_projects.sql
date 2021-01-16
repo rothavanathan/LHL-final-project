7e31013ca384947935937d7c09720a898a7c56ad
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 17:15:59 -0500

    add all seed data

---
+++ server/src/db/seeds/03_projects.sql
@@ -0,0 +1,5 @@
+insert into projects (id, user_id, title, notes) values (1, 1, 'Burial Ground for Charlie', 'This one has the weird tuning E-A-D-F#-B-E');
+insert into projects (id, user_id, title, notes) values (2, 1, 'Assimilated', 'Tern, white-winged black');
+insert into projects (id, user_id, title, notes) values (3, 2, 'intangible', 'Prehensile-tailed porcupine');
+insert into projects (id, user_id, title, notes) values (4, 3, 'contextually-based', 'Cape fox');
+insert into projects (id, user_id, title, notes) values (5, 3, 'Organic', 'Mongoose, yellow');
