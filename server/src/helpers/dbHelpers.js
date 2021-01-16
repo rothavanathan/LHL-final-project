56bb10051ce3ec1430087884b643794b7ef91431
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 17:12:53 -0500

    start refractor on functions

--- server/src/helpers/dbHelpers.js
+++ server/src/helpers/dbHelpers.js
@@ -1,4 +1,7 @@
+//  Where the query functionality will lie
+
 module.exports = (db) => {
+  // Gets all users
   const getUsers = () => {
       const query = {
           text: 'SELECT * FROM users',
@@ -9,7 +12,7 @@ module.exports = (db) => {
           .then((result) => result.rows)
           .catch((err) => err);
   };
-
+  // Finds user by email
   const getUserByEmail = email => {
 
       const query = {
@@ -22,19 +25,19 @@ module.exports = (db) => {
           .then(result => result.rows[0])
           .catch((err) => err);
   }
-
-  const addUser = (firstName, lastName, email, password) => {
+  // Adds user to db
+  const addUser = (firstName, email, password) => {
       const query = {
-          text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *` ,
-          values: [firstName, lastName, email, password]
+          text: `INSERT INTO users (first_name, email, password) VALUES ($1, $2, $3) RETURNING *` ,
+          values: [firstName, email, password]
       }
 
       return db.query(query)
           .then(result => result.rows[0])
           .catch(err => err);
   }
-
-  const getUsersPosts = () => {
+  // Gets users by projects, unsure about functionality rn - not working
+  const getUsersByProjects = () => {
       const query = {
           text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
       FROM users
@@ -48,10 +51,41 @@ module.exports = (db) => {
 
   }
 
+  // Gets users by collections, unsure about functionality rn - not working
+  const getUsersByCollections = () => {
+    const query = {
+        text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
+    FROM users
+    INNER JOIN posts
+    ON users.id = posts.user_id`
+    }
+
+    return db.query(query)
+        .then(result => result.rows)
+        .catch(err => err);
+
+  }
+  // 
+  const getSongByProject = () => {
+    const query = {
+        text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
+    FROM users
+    INNER JOIN posts
+    ON users.id = posts.user_id`
+    }
+
+    return db.query(query)
+        .then(result => result.rows)
+        .catch(err => err);
+
+  }
+
   return {
       getUsers,
       getUserByEmail,
       addUser,
-      getUsersPosts
+      getUsersByProjects,
+      getUsersByCollections, 
+      getSongByProject
   };
 };
