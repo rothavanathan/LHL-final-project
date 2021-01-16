a8d5b6e392dbeaf3edb146fd50c1a57068317c1e
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 12:37:07 -0500

    rename routes, change var to const

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var path = require("path");
-var cors = require("cors");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const stemsRouter = require("./src/routes/stems");
+const usersRouter = require("./src/routes/users");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(cors());
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/stems", stemsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
