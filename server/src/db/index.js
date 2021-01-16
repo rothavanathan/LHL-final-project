3c7267cb9d9496da317eed3ad2e9699d035d23b4
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 16:36:13 -0500

    setup db pg connection

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,12 +1,16 @@
 // This file handles all our db interactions
 const pg = require("pg");
+require('dotenv').config();
+
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable` ;
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  connectionString: connectionString || process.env.DATABASE_URL,
 });
 
 client
   .connect()
+  .then(console.log("Connected to db"))
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
