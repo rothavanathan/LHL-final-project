d7c153c4cf67fa7d8a97db51af9943738c8f814b
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 13:08:55 -0500

    add helpers folder and modify route names

--- server/src/routes/stems.js
+++ server/src/routes/stems.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET stem info from db */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
