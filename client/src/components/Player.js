ec621bf5dab44f3547c4e1b57b701fec6c10b64b
Laura Tannahill <lntannahill@gmail.com>
2021-01-16 12:35:52 -0400

    Add button to control playing and create emitter to listen to its click and set the state of play

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Waveform from "./Waveform";
+import Emitter from "../EventEmitter"
 
 const AudioCtx = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioCtx();
 
 const Player = ({ tracks }) => {
+  const [playing, setPlay] = useState(false);
+   const handlePlayPause = () => {
+    setPlay(!playing);
+    Emitter.emit('click', setPlay(playing));
+  };
   return (
     <div className="player">
+      <button onClick={handlePlayPause}>{!playing ? "Play" : "Pause"}</button>
       {tracks.map((track, i) => {
         return <Waveform key={i} url={track.url} context={audioCtx} />
       })};
