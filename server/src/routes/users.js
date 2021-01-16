e4441aa94b2ff4dfa4e8e58c86b0d19d44ef7e26
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-16 17:14:59 -0500

    refractor routes user

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,14 +1,76 @@
+// const express = require('express');
+// const router = express.Router();
+
+// /* GET user info from db */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// /* POST user info from db */
+// router.post('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
+const {
+    getPostsByUsers
+} = require('../helpers/dataHelpers');
 
-/* GET user info from db */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = ({
+    getUsers,
+    getUserByEmail,
+    addUser,
+    getUsersPosts
+}) => {
+    /* GET users listing. */
+    router.get('/', (req, res) => {
+        getUsers()
+            .then((users) => res.json(users))
+            .catch((err) => res.json({
+                error: err.message
+            }));
+    });
 
-/* POST user info from db */
-router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+    // router.get('/posts', (req, res) => {
+    //     getUsersPosts()
+    //         .then((usersPosts) => {
+    //             const formattedPosts = getPostsByUsers(usersPosts);
+    //             res.json(formattedPosts);
+    //         })
+    //         .catch((err) => res.json({
+    //             error: err.message
+    //         }));
+    // });
 
-module.exports = router;
+    router.post('/', (req, res) => {
+
+        const {
+            first_name,
+            email,
+            password
+        } = req.body;
+
+        getUserByEmail(email)
+            .then(user => {
+
+                if (user) {
+                    res.json({
+                        msg: 'Sorry, a user account with this email already exists'
+                    });
+                } else {
+                    return addUser(first_name, email, password)
+                }
+
+            })
+            .then(newUser => res.json(newUser))
+            .catch(err => res.json({
+                error: err.message
+            }));
+
+    })
+
+    return router;
+};
