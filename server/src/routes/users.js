52aee89e3483f56d3cdba168f2d9e266f32b7f9a
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-17 15:08:10 -0500

    add get route for users, wokring

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,76 +1,19 @@
-// const express = require('express');
-// const router = express.Router();
-
-// /* GET user info from db */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// /* POST user info from db */
-// router.post('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// module.exports = router;
+// Methods in helpers to be incorporated here too
 
 const express = require('express');
 const router = express.Router();
-const {
-    getPostsByUsers
-} = require('../helpers/dataHelpers');
 
-module.exports = ({
-    getUsers,
-    getUserByEmail,
-    addUser,
-    getUsersPosts
-}) => {
-    /* GET users listing. */
-    router.get('/', (req, res) => {
-        getUsers()
-            .then((users) => res.json(users))
-            .catch((err) => res.json({
-                error: err.message
-            }));
-    });
-
-    // router.get('/posts', (req, res) => {
-    //     getUsersPosts()
-    //         .then((usersPosts) => {
-    //             const formattedPosts = getPostsByUsers(usersPosts);
-    //             res.json(formattedPosts);
-    //         })
-    //         .catch((err) => res.json({
-    //             error: err.message
-    //         }));
-    // });
-
-    router.post('/', (req, res) => {
-
-        const {
-            first_name,
-            email,
-            password
-        } = req.body;
-
-        getUserByEmail(email)
-            .then(user => {
-
-                if (user) {
-                    res.json({
-                        msg: 'Sorry, a user account with this email already exists'
-                    });
-                } else {
-                    return addUser(first_name, email, password)
-                }
-
-            })
-            .then(newUser => res.json(newUser))
-            .catch(err => res.json({
-                error: err.message
-            }));
-
-    })
-
-    return router;
+module.exports = (db) => {
+  /* GET all users*/
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then((data) => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
 };
