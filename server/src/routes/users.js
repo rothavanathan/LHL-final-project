8be0cafe3f32dc76ee56558353f261215df577f8
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-17 15:23:24 -0500

    exp with getUserBy email

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,5 +1,7 @@
 // Methods in helpers to be incorporated here too
 
+// addUser, getby email, get by collection
+
 const express = require('express');
 const router = express.Router();
 
@@ -15,5 +17,23 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+
+//   router.get("/:id", (req, res) => {
+
+//     const query = {
+//         text: `SELECT * FROM users WHERE email = $1` ,
+//         values: [email]
+//     }
+
+//     db.query(query)
+//       .then((data) => {
+//         const users = data.rows;
+//         res.json({ users });
+//       })
+//       .catch((err) => {
+//         res.status(500).json({ error: err.message });
+//       });
+//   });
+
   return router;
 };
