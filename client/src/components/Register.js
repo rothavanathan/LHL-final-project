0f025009464f309579fd4105681be28793554735
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-17 16:51:32 -0500

    register logic started

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,19 +1,57 @@
 //for development -> click through to test userflows
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Register(props) {
+  const [formData, setFormData] = useState("");
+  const [user, setUser] = useState({
+    first_name: "",
+    email: "",
+    password: ""
+  });
+  
+    useEffect(() => {
+      saveUser()
+    }, []);
+
+  const saveUser = () => {
+
+    axios
+      .put(`http://localhost:8000/api/users`, formData)
+      .then((res) => {
+        console.log(`from get request`, res.data.users)
+        setUser(res.data.users);
+      })
+      .catch((err) => console.log(err));
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    saveUser() // Save games when form is submitted
+  }
+
+  const handleChange = (event) => {
+    setFormData(event.target.value)
+  }
+
   return (
     <div>
       <h1>I AM Register</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
           type="first_name"
+          value={formData}
           name="first_name"
           placeholder="Gary"
           aria-label="first_name"
         ></input>
 
         <input
+          value={formData}
+          onChange={handleChange}
           type="email"
           name="email"
           placeholder="example@gmail.com"
@@ -21,6 +59,8 @@ export default function Register(props) {
         ></input>
 
         <input
+          value={formData}
+          onChange={handleChange}
           type="password"
           name="password"
           placeholder="your password here"
