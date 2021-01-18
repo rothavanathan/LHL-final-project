84a1dfdee26d4bd1084834832836b1d0048a8f13
Laura Tannahill <lntannahill@gmail.com>
2021-01-18 19:05:23 -0400

    Add request for projects

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,15 +1,26 @@
-import { Link, Redirect } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
+import axios from 'axios';
 
 import Player from "./Player";
 import ProjectNav from "./ProjectNav";
 
 export default function Project(props) {
   const { isLoggedIn } = props;
+  const {id} = useParams()
+
+  axios
+    .get(`api/project/${id}`)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+
+
   return isLoggedIn ? (
     <div>
       <Link to="home">Back to Home</Link>
       <h1>Burial Ground - Charlie learning guitar part</h1>
-      <Player tracks={props.tracks}></Player>
+      <p>Project Id is {id}</p>
+      <Player tracks={[]}></Player>
       <ProjectNav />
     </div>
   ) : (
