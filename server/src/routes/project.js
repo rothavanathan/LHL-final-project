55e3c693a520b5f15c06320f07717b38a7271eac
Laura Tannahill <lntannahill@gmail.com>
2021-01-19 12:35:14 -0400

    Add put request to update note by project id

--- server/src/routes/project.js
+++ server/src/routes/project.js
@@ -21,5 +21,36 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+
+    // ADD NOTE TO PROJECT BY ID
+    router.put("/", (req, res) => {
+      const query = `
+        UPDATE projects
+        SET notes = $1
+        WHERE id = $2;
+      `;
+
+      const { notes, id } = req.body;
+
+      console.log(notes, id);
+
+      return db
+        .query(query, [notes, id])
+        .then((data) => {
+          console.log(`insert completed!`, data);
+          // const users = data.rows;
+          // req.session.userId = users[0].id;
+          // res.json({ users });
+          console.log("DATA----", data)
+          console.log("ROWS-----", data.rows);
+        })
+        .catch((err) => {
+          console.log(`ruh roh`, err);
+          res.status(500).json({ error: err.message });
+        });
+    });
+
   return router;
+
+
 };
