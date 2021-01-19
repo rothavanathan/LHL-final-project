66d09d07e4d7878386bdd0ec4381d9a158378292
Laura Tannahill <lntannahill@gmail.com>
2021-01-19 15:28:37 -0400

    Remove some logs

--- server/src/routes/project.js
+++ server/src/routes/project.js
@@ -26,11 +26,6 @@ module.exports = (db) => {
       return db
         .then((data) => {
           console.log(`insert completed!`, data);
-          // const users = data.rows;
-          // req.session.userId = users[0].id;
-          // res.json({ users });
-          console.log("DATA----", data)
-          console.log("ROWS-----", data.rows);
         })
         .catch((err) => {
           console.log(`ruh roh`, err);
