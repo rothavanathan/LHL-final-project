c5191c197e2e67cc28e17b871f0df5f8c711855c
Laura Tannahill <lntannahill@gmail.com>
2021-01-20 09:37:24 -0400

    Add grid for styling

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -1,29 +1,68 @@
 import React from "react";
-import { Card } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Grid, Typography, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import classnames from "classnames";
 
+const useStyles = makeStyles((theme) => ({
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
 export default function Album(props) {
-  const albumInfoClass = classnames("album__info", {
-    "album__info--explicit": props.collectionExplicitness === "explicit"
-  });
+  // const albumInfoClass = classnames("album__info", {
+  //   "album__info--explicit": props.collectionExplicitness === "explicit"
+  // });
 
+  const classes = useStyles();
+
+  // return (
+  //   <Card
+  //     variant="elevation"
+  //     raised="true"
+
+  //   >
+  //     <article className="album">
+  //       <img className="album__thumbnail" src={props.artworkUrl100} alt="Album" />
+  //       <div className={albumInfoClass}>
+  //         <div className="song__name">{props.trackName}</div>
+  //         <div className="album__name">{props.collectionName}</div>
+  //         <div className="album__artist">{props.artistName}</div>
+  //       </div>
+  //       <div>Hamburger</div>
+  //     </article>
+
+  //   </Card>
+  // );
   return (
-    <Card
-      variant="elevation"
-      raised="true"
-
-    >
-      <article className="album">
-        <img className="album__thumbnail" src={props.artworkUrl100} alt="Album" />
-        <div className={albumInfoClass}>
-          <div className="song__name">{props.trackName}</div>
-          <div className="album__name">{props.collectionName}</div>
-          <div className="album__artist">{props.artistName}</div>
-        </div>
-        <div>Hamburger</div>
-      </article>
-
+    <Grid item key={props.key} xs={12} sm={6} md={4}>
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.cardMedia}
+        image={props.artworkUrl100}
+        title={props.trackName}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+        {props.trackName}
+        </Typography>
+        <Typography>
+          {props.artistName}
+        </Typography>
+        <Typography>
+          {props.collectionName}
+        </Typography>
+      </CardContent>
     </Card>
-  );
+  </Grid>
+  )
 }
