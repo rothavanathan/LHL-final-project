a222b84f26274dc7821e0f57be077f99cf3a6a1d
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-21 17:31:50 -0500

    cleaup code

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
-import axios from "axios";
-import { makeStyles } from '@material-ui/core/styles';
-import { FormControl, Button } from '@material-ui/core';
-
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
 
 export default function Entry(props) {
   const useStyles = makeStyles((theme) => ({
-
     entryImage: {
       backgroundImage: `url(${"https://unsplash.com/photos/MEL-jJnm7RQ"})`,
     },
@@ -17,14 +13,13 @@ export default function Entry(props) {
       flexDirection: "column",
       alignItems: "center",
       width: "100%",
-      height: "50%"    
+      height: "50%",
     },
 
     heading: {
       color: "antiquewhite",
       fontFamily: "Noto Sans",
       margin: "50px",
-
     },
 
     heading2: {
@@ -32,52 +27,46 @@ export default function Entry(props) {
       fontFamily: "Noto Sans",
       fontSize: 15,
       margin: "40px",
-
     },
 
     logButton: {
       fontFamily: "Noto Sans",
-      display: 'flex',
+      display: "flex",
       background: "#044a10",
       width: "60%",
       margin: "25px",
-      color: "antiquewhite"
+      color: "antiquewhite",
     },
 
     back: {
       flexGrow: 1,
-      display: 'flex',
+      display: "flex",
     },
-
   }));
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={classes.entryImage}>
-
       <h1>LOGO</h1>
 
       <h1 className={classes.heading}>Layers</h1>
 
-      <h2 className={classes.heading2}>With Layers you can isolate the parts of a track to teach or learn. Drums, guitar, vocals, or a combination of a few? It's really up to you.</h2>
+      <h2 className={classes.heading2}>
+        With Layers you can isolate the parts of a track to teach or learn.
+        Drums, guitar, vocals, or a combination of a few? It's really up to you.
+      </h2>
 
       <div className={classes.formDiv}>
-        <Button to="/register" component={Link}
-          className={classes.logButton} 
-          >Sign up
-        {/* <Link  className={classes.regLink}>
-        </Link> */}
+        <Button to="/register" component={Link} className={classes.logButton}>
+          Sign up
         </Button>
 
-        <Button to="/login" component={Link}
-          className={classes.logButton} 
-          >Login
-        {/* <Link  className={classes.regLink}>
-        </Link> */}
+        <Button to="/login" component={Link} className={classes.logButton}>
+          Login
         </Button>
       </div>
-    
     </div>
-  )
+  );
 }
+
