33c1a6743ef2d9dce2a60ff3c08c3147b8e46e8a
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-20 21:02:56 -0330

    grab users collections so they can attach a project

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -9,36 +9,58 @@ import Notes from "./Notes"
 export default function Project(props) {
   const [content, setContent] = useState([{ title: "", artist: "", url: "" }])
   const [collections, setCollections] = useState([{ name: "", user_id: "", thumbnail: "" }])
+  const [collectionId, setCollectionId] = useState()
 
   const { isLoggedIn } = props;
   const { id } = useParams()
 
   useEffect(() => {
     axios
+      //grabbing project, and associated songs/stems
       .get(`http://localhost:8000/api/project/${id}`)
-      .then(() => {
-        axios.get('/api/content')
-          .then(data => setContent(data.data.projects))
+      .then((data) => {
+        axios
+          //grabbing collections for user
+          .get('/api/content')
+          .then(data2 => {
+            console.log(`grabbing content for the collections`, data2.data.collections);
+            setContent(data.data.projects);
+            console.log(`project data`, data.data.projects[0].collection_id)
+            setCollectionId(data.data.projects[0].collection_id);
+            setCollections(data2.data.collections)
+          })
+          .then(data2 => {
+
+            setContent(data.data.projects)
+          })
           .catch(err => console.log(err))
       })
   }, [])
 
 
 
-  console.log("CONTENT----------", content);
+  // console.log("CONTENT----------", content);
 
   const stems = content.map((project) => {
     const { title, url, icon, peaks_array, name } = project
     return { title, url, icon, peaks_array, name }
   })
 
-  console.log("I AM CONTENT ZERO DO I HAVE A NOTE", content[0].notes);
+  // console.log("I AM CONTENT ZERO DO I HAVE A NOTE", content[0].notes);
 
   return isLoggedIn ? (
     <div>
       <Link to="/home">Back to Home</Link>
       <h1>{content[0].title}</h1>
       <h2>{content[0].artist}</h2>
+      <h3>collections</h3>
+      <ul>
+        {collections.map(collection => {
+          return <li>{collection.name}</li>
+        })}
+      </ul>
+
+
       <Player tracks={stems}></Player>
       {content[0] && <Notes projectId={id} existingNote={content[0].notes} />}
       <ProjectNav />
