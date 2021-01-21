6e386e25ddd7e113a6f63b6792f83ecf77652bbc
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-21 17:33:01 -0500

    cleaup code

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
-import { makeStyles } from '@material-ui/core/styles';
-import { FormControl, Button } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import { FormControl, Button } from "@material-ui/core";
 
 export default function Login(props) {
   const { setUser, isLoggedIn } = props;
@@ -36,16 +36,14 @@ export default function Login(props) {
   };
 
   const useStyles = makeStyles((theme) => ({
-
     formDiv: {
-      height: "60%"
+      height: "60%",
     },
 
     heading: {
       color: "antiquewhite",
       fontFamily: "Noto Sans",
       margin: "50px",
-
     },
 
     heading2: {
@@ -53,7 +51,6 @@ export default function Login(props) {
       fontFamily: "Noto Sans",
       fontSize: 15,
       margin: "40px",
-
     },
 
     form: {
@@ -61,78 +58,72 @@ export default function Login(props) {
       flexDirection: "column",
       alignItems: "center",
       width: "100%",
-      height: "50%"
+      height: "50%",
     },
 
     email: {
-      width: '80%',
+      width: "80%",
       color: "white",
-      display: 'flex',
-      flexDirection: 'column',
+      display: "flex",
+      flexDirection: "column",
     },
 
     emailText: {
       color: "white",
       display: "flex",
-      backgroundColor:" #000821",
+      backgroundColor: " #000821",
       border: "none",
-      borderBottom:" #3b2c13 4px solid",
+      borderBottom: " #3b2c13 4px solid",
       backgroundImage: `url(${"https://www.transparenttextures.com/patterns/otis-redding.png"})`,
       margin: "30px",
       outline: "none",
       alignSelf: "center",
       width: "100%",
       fontSize: "15px",
-
-
     },
 
     password: {
-      width: '80%',
+      width: "80%",
       color: "white",
-      display: 'flex',
-      flexDirection: 'column',
-      
+      display: "flex",
+      flexDirection: "column",
     },
 
     passwordText: {
       color: "white",
       display: "flex",
-      backgroundColor:" #000821",
+      backgroundColor: " #000821",
       border: "none",
-      borderBottom:" #3b2c13 4px solid",
+      borderBottom: " #3b2c13 4px solid",
       backgroundImage: `url(${"https://www.transparenttextures.com/patterns/otis-redding.png"})`,
       margin: "30px",
       outline: "none",
       width: "100%",
       alignSelf: "center",
       fontSize: "15px",
-
-
     },
 
     logButton: {
       fontFamily: "Noto Sans",
-      display: 'flex',
+      display: "flex",
       background: "#044a10",
       width: "60%",
       margin: "40px",
-      color: "antiquewhite"
+      color: "antiquewhite",
     },
 
     back: {
       flexGrow: 1,
-      display: 'flex',
+      display: "flex",
     },
 
     regLink: {
       color: "antiquewhite",
-      textDecoration: "none"
-    }
-
+      textDecoration: "none",
+    },
   }));
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   return !isLoggedIn ? (
     <div>
@@ -140,9 +131,10 @@ export default function Login(props) {
       <h2 className={classes.heading2}>Let's get you logged in</h2>
 
       <div className={classes.formDiv}>
-        <form  className={classes.form} onSubmit={handleSubmit}>
+        <form className={classes.form} onSubmit={handleSubmit}>
           <FormControl className={classes.emailInput}>
-            <input className={classes.emailText}
+            <input
+              className={classes.emailText}
               value={emailData}
               onChange={handleEmail}
               type="email"
@@ -153,7 +145,8 @@ export default function Login(props) {
           </FormControl>
 
           <FormControl className={classes.passwordInput}>
-            <input className={classes.passwordText}
+            <input
+              className={classes.passwordText}
               value={passwordData}
               onChange={handlePassword}
               type="password"
@@ -163,16 +156,18 @@ export default function Login(props) {
             ></input>
           </FormControl>
 
-          <Button 
-            className={classes.logButton} 
-            type="submit"
-            >Login
+          <Button className={classes.logButton} type="submit">
+            Login
           </Button>
         </form>
-          <Link to="/register" className={classes.regLink}>
-             <href className={classes.regLink}>Don't have an account? Register here</href>
-          </Link>
+        <Link to="/register" className={classes.regLink}>
+          <href className={classes.regLink}>
+            Don't have an account? Register here
+          </href>
+        </Link>
       </div>
     </div>
-  ) : <Redirect to="/home"/> ;
+  ) : (
+    <Redirect to="/home" />
+  );
 }
