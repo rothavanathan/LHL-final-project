c8b965c432c8536a8e0a13b3129ef1d73d24ceab
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-23 17:14:50 -0330

    edit burial ground fullSong seed data

--- server/db/seeds/04_songs.sql
+++ server/db/seeds/04_songs.sql
@@ -1,5 +1,5 @@
 --1
-insert into songs (artist, title, album, url_album_artwork, url_full_song_preview, duration ) values ('Wily', 'Burial Ground', 'Possessions - EP', 'https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/55/e5/30/55e53045-6af0-04df-6b2c-2d69e09432de/source/600x600bb.jpg', 'https://rykabrown.com/wp-content/uploads/2021/01/BURIAL_GROUND_piano.mp3', '470');
+insert into songs (artist, title, album, url_album_artwork, url_full_song_preview, duration ) values ('Wily', 'Burial Ground', 'Possessions - EP', 'https://rykabrown.com/wp-content/uploads/2021/01/BURIAL-GROUND_fullmix-faux-mastered.mp3', '470');
 
 --2
 insert into songs (artist, title, album, url_album_artwork, url_full_song_preview, duration ) values ('Phoenix', 'Lisztomania', 'Wolfgang Amadeus Phoenix', 'https://is3-ssl.mzstatic.com/image/thumb/Music114/v4/8e/23/3e/8e233eb4-d7ae-1570-b933-1178ea30f5b0/source/600x600bb.jpg', 'https://rykabrown.com/wp-content/uploads/2021/01/Lisztomania_fullSong.mp3', '470');
