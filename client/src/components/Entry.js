7de8665ca67b550ebcb51a3197787bd869839eb9
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-23 11:42:14 -0500

    more cleanup

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -1,4 +1,4 @@
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 
